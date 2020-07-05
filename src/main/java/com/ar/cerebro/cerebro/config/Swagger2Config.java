5267ec030dbc5f15b90cc117b346f26fde107c92
Wizzac <13040359+wizzac@users.noreply.github.com>
2020-07-05 20:36:26 -0300

    Swagger!

--- src/main/java/com/ar/cerebro/cerebro/config/Swagger2Config.java
+++ src/main/java/com/ar/cerebro/cerebro/config/Swagger2Config.java
@@ -1,9 +1,14 @@
 package com.ar.cerebro.cerebro.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,21 +17,31 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
 
+
     @Bean
     public Docket api() {
-            return new Docket(DocumentationType.SWAGGER_2).select()
-                    .apis(RequestHandlerSelectors
-                            .basePackage("com.ar.cerebro.cerebro.controller"))
-                    .paths(PathSelectors.regex("/.*"))
-                    .build().apiInfo(apiEndPointsInfo());
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.ar.cerebro.cerebro"))
+                .paths(PathSelectors.regex("/.*")).build().apiInfo(apiEndPointsInfo())
+                .enable(true);
     }
+
     private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder().title("Spring Boot REST API")
-                .version("1.0.0")
-                .build();
+        return new ApiInfoBuilder().title("Spring Boot REST API").version("1.0.0").build();
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
     }
 }
